c702e7f545b736de0c44b908f317419b18718bb9
takumibaba <s09704tb@gmail.com>
2012-02-07 23:51:42 +0900

    elsif

--- server.rb
+++ server.rb
@@ -5,9 +5,9 @@ require "sinatra"
 get "/:id/:state" do 
   if(params[:state] ==0){
     "着信なうだよー"
-  }else if(params[:state] == 1){
+  }elsif(params[:state] == 1){
     "電話がきれたよー"
-  }else if(params[:state] == 2){
+  }elsif(params[:state] == 2){
     "電話をとったよー"
   }
 end
