7f8741f82255bf40c63e1597c9a010f6c17b0e0b
takumibaba <s09704tb@gmail.com>
2012-02-16 16:40:43 +0900

    コネクションが確率したら拡張機能へとメッセージ送信する機能の部分

--- server.js
+++ server.js
@@ -5,4 +5,30 @@
  * Time: 21:43
  * To change this template use File | Settings | File Templates.
  *
+ *
+ *
+**/
 
+var user = {};
+
+var io = require('socket.io').listen(8080);
+
+io.sockets.on('connection', function (socket) {
+    //user.push(socket);
+    console.log(socket);
+   
+    socket.on('call',function(msg){
+        socket.emit("call","hoge");
+        socket.broadcast.emit("call","hoge");
+        console.log("着信！");
+    });
+
+
+    socket.on('discall',function(){
+        console.log("着信切れたよ。");
+    });
+
+    socket.on('disconnect',function(){
+        console.log("着信切れたよ。");
+    });
+});
